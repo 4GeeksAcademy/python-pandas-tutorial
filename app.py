e09e168e59fae86bb5ce30ca538de3a8aa083416
Martín Suárez <ms.prgramador@gmail.com>
2022-04-20 22:05:23 +0000

    ex 05

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import pandas as pd
+# two dimensional array of name,age values.
+data = [["Toyota", "Corolla", "Blue"], ["Ford", "K", "Yellow"], ["Porche", "Cayenne", "White"]]
 
-my_series = pd.Series([2, 4, 6, 8, 10])
-modified_series = my_series.apply(lambda x:x/2)
-print(modified_series)
+# create the dataframe and name the columns
+df = pd.DataFrame(data,columns=['Brand', 'Make', 'Color'])
+
+# print the dataframe
+print(df)
