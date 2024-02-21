25e3e0479cc3b630a34363393a7939ae0f60a4c5
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-02-21 10:02:12 +0100

    Update solution.hide.py

--- .learn/exercises/06.1-remove-column/solution.hide.py
+++ .learn/exercises/06.1-remove-column/solution.hide.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 data_frame = pd.read_csv('.learn/assets/us_baby_names_right.csv')
-# data_frame = pd.iloc[: , 1:]
 del data_frame[data_frame.columns[0]]
-print(data_frame.head(5))
 
-# https://thispointer.com/pandas-delete-first-column-of-dataframe-in-python/
+print(data_frame.head(5))
