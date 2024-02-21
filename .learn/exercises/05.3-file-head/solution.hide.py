b0517a4dec2beb7eb0c3056509c27eee2f48ce71
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-02-21 07:22:09 +0100

    Update solution.hide.py

--- .learn/exercises/05.3-file-head/solution.hide.py
+++ .learn/exercises/05.3-file-head/solution.hide.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 data_frame = pd.read_csv('.learn/assets/pokemon_data.csv')
 
-# we can read just the first 3 rows
+# We can read only the first 3 rows
 print(data_frame.head(3))
