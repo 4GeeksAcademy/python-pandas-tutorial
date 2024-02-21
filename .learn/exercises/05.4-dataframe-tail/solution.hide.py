12d0cb6f24f80e839ac46107c3cf74a73188845c
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-02-21 08:05:07 +0100

    Update solution.hide.py

--- .learn/exercises/05.4-dataframe-tail/solution.hide.py
+++ .learn/exercises/05.4-dataframe-tail/solution.hide.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 data_frame = pd.read_csv('.learn/assets/pokemon_data.csv')
 
-# we can read just the first 3 rows
+# We can read only the last 3 rows
 print(data_frame.tail(3))
